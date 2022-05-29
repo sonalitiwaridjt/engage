8a5c31c6e31cd1dd4c8360fb3dea69504a9f3914
sonalitiwaridjt <101213834+sonalitiwaridjt@users.noreply.github.com>
2022-05-29 17:06:02 +0530

    using proper routes

--- engage/engage/urls.py
+++ engage/engage/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('goal.urls')),
+    path('', include('goal.urls')),
     path('goal/', include('goal.urls')),
     path('admin/', admin.site.urls),
 ]
